5640f30e3108382a18d7ffe2111bc7d5d3c2346c
Rose Mullan <rfmullan@gmail.com>
2023-05-11 18:47:32 +0100

    setup hosting

--- listen.js
+++ listen.js
@@ -1,7 +1,4 @@
-console.log('in listen.js');
 const app = require('./app.js');
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
-  if (err) console.log(err);
-  else console.log('Listening on port 9090...');
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
